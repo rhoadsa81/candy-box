db6c0b03f820e7fbf2f798d249b1dc9e7e7e6405
Jonathan Arnett <jonarnett90@gmail.com>
2015-10-06 14:59:36 -0400

    Added boilerplate

---
+++ js/adapter.js
@@ -0,0 +1,25 @@
+var GAME = {
+    addButton: function(id, buttonText, clickFunction) {
+        $(".candy-section").append("<button id=\"" + id + "\">" + buttonText + "</button>");
+        $("#" + id).on("click", clickFunction)
+    },
+    addTextBox: function(id, text) {
+        $(".candy-section").append("<p id=\"" + id + "\">" + text + "</p>");
+    },
+    removeElement: function(id) {
+        $("#" + id).remove();
+    },
+    removeHandler: function(id, handler) {
+        $("#" + id).off("click", handler);
+    },
+    addHandler: function(id, handler) {
+        $("#" + id).on("click", handler)
+    },
+    changeHandler: function(id, oldFunction, newFunction) {
+        this.removeHandler(id, oldFunction);
+        this.addHandler(id, newFunction);
+    },
+    setText: function(id, text) {
+        $("#" + id).html(text);
+    }
+};
---
+++ js/game.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    // The world is yours to explore!
+
+});
