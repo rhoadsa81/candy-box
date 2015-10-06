337355974b27c973abbb2de6a065cc547208f644
rhoadsa81 <rhoadsag@gmail.com>
2015-10-06 19:47:25 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     // The world is yours to explore!
     GAME.candies=0;
     GAME.setText('candy-counter', 'You have' + GAME.candies +'candies');
+    
+    GAME.getMoreCandies():
+    GAME.setTExt('candy-counter', 'You have' + GAME.candies +'candies');
 });
+ function getMoreCandies(){
+     GAME.candies = GAME.candies +1;
+ }
